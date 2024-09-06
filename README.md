1dc8cce7ee2b5ec9d8badf8f817bea81361eb63b
claraaurelia <clarasetiady@gmail.com>
2024-09-06 11:34:39 +0700

    tugas individu 2

--- README.md
+++
@@ -1,3 +0,0 @@
-Nama : Clara Aurelia Setiady
-NPM : 2306217304
-Kelas : PBP C
---
+++ main/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+class Product(models.Model):
+    product_name = models.CharField(max_length=255)
+    product_price = models.IntegerField
+    product_description = models.TextField
+# Create your models here.
+    @property
+    def is_product_expensive(self):
+        return self.product_description > 100000
